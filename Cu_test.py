14e08d5ebc6a09d7747b562c9478e9ff67305c5c
Laurent Mercadier <laurent.mercadier@xfel.eu>
2022-02-08 21:52:09 +0100

    Adds AI table and uses Cu ground state

--- Cu_test.py
+++ Cu_test.py
@@ -1,19 +1,28 @@
 from pfac import fac
 
 fac.SetAtom('Cu')
-fac.Closed('1s', '2s', '3s')
-fac.Config('3p6 2p6 3d9 4s1', group='upper')
-fac.Config('3p6 2p5 3d10 4s1', group='lower')
+fac.Closed('1s', '2s', '3s', '3p')
+fac.Config('2p6 3d10 4s1', group='ground')
+fac.Config('2p6 3d9 4s1', group='lower')
+fac.Config('2p5 3d10 4s1', group='upper')
+fac.Config('2p6 3d8 4s1', group='lower_AI')
 
 name = 'Cu_test'
 fac.ConfigEnergy(0)
-fac.OptimizeRadial('lower')
+fac.OptimizeRadial('ground')
 fac.ConfigEnergy(1)
 print('ConfigEnergy and optimizeRadial done.')
-fac.Structure(name+'.lev.b', ['upper', 'lower'])
+fac.Structure(name+'.lev.b', ['ground', 'upper',
+                              'lower', 'lower_AI',])
 fac.MemENTable(name+'.lev.b')
 fac.PrintTable(name+'.lev.b', name+'.lev', 1)
 print('Level table saved.')
 fac.TransitionTable(name+'.tr.b', ['lower'], ['upper'])
 fac.PrintTable(name+'.tr.b', name+'.tr', 1)
 print('Transition table saved.')
+fac.AITable(name+'.ai.b', ['upper'], ['lower_AI'])
+fac.PrintTable(name+'.ai.b', name+'.ai', 1)
+print('AI table saved.')
+
+print(fac.AIBranch(name+'.ai.b', 21, 5))
+print(fac.AIBranch(name+'.ai.b', 21, 12))

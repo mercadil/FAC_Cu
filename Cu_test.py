84fa6e5f2aee9107b8103f541b033eb10df23a98
Laurent Mercadier <laurent.mercadier@xfel.eu>
2022-02-23 20:37:45 +0100

    ladders 0 and 1, transition table for ii1 and ii2

--- Cu_test.py
+++ Cu_test.py
@@ -1,11 +1,11 @@
 from pfac import fac
 
 fac.SetAtom('Cu')
-fac.Closed('1s', '2s', '3s')
+fac.Closed('1s', '2s', '3s',)
 fac.Config('2p6 3p6 4s1 3d10', group='ik0_ii0')
 fac.Config('2p6 3p6 4s1 3d9', group='ik0_ii1')
-fac.Config('2p6 3p6 4s1 3d7', group='ik0_ii3')
 fac.Config('2p6 3p6 4s1 3d8', group='ik0_ii2')
+fac.Config('2p6 3p6 4s1 3d7', group='ik0_ii3')
 fac.Config('2p6 3p6 4s1 3d6', group='ik0_ii4')
 fac.Config('2p6 3p6 4s1 3d5', group='ik0_ii5')
 fac.Config('2p6 3p6 4s1 3d4', group='ik0_ii6')
@@ -24,7 +24,7 @@ fac.Config('2p5 3p6 4s1 3d4', group='ik1_ii7')
 fac.Config('2p5 3p6 4s1 3d3', group='ik1_ii8')
 fac.Config('2p5 3p6 4s1 3d2', group='ik1_ii9')
 fac.Config('2p5 3p6 4s1 3d1', group='ik1_ii10')
-
+'''
 fac.Config('2p5 3p5 4s1 3d10', group='ik2_ii2')
 fac.Config('2p5 3p5 4s1 3d9', group='ik2_ii3')
 fac.Config('2p5 3p5 4s1 3d8', group='ik2_ii4')
@@ -62,13 +62,19 @@ fac.Config('2p5 3p3 4s1 3d7', group='ik5_ii7')
 fac.Config('2p5 3p3 4s1 3d6', group='ik5_ii8')
 fac.Config('2p5 3p3 4s1 3d5', group='ik5_ii9')
 fac.Config('2p5 3p3 4s1 3d4', group='ik5_ii10')
+'''
 
-ladders = {0: 0, 1: 1, 2: 2, 3: 3, 4: 1, 5: 4}
+#ladders = {0: 0, 1: 1, 2: 2, 3: 3, 4: 1, 5: 4}
+ladders = {0: 0, 1: 1, }
 
 all_groups = ["ik%d_ii%d"%(k, i) for k in ladders for
                i in range(ladders[k], 11)]
+#all_groups = ['ik0_ii0', 'ik0_ii1', 'ik1_ii1', 'ik1_ii2']
+print(all_groups)
 ii1_low = ['ik0_ii1']
 ii1_up = ['ik1_ii1']
+ii2_low = ['ik0_ii2']
+ii2_up = ['ik1_ii2']
 
 name = 'Cu_test'
 fac.ConfigEnergy(0)
@@ -79,7 +85,8 @@ fac.Structure(name+'.lev.b', all_groups)
 fac.MemENTable(name+'.lev.b')
 fac.PrintTable(name+'.lev.b', name+'.lev', 1)
 print('Level table saved.')
-fac.TransitionTable(name+'.tr.b', [ii1_low], [ii1_up])
+fac.TransitionTable(name+'.tr.b', ii1_low, ii1_up)
+fac.TransitionTable(name+'.tr.b', ii2_low, ii2_up)
 fac.PrintTable(name+'.tr.b', name+'.tr', 1)
 print('Transition table saved.')
 #fac.AITable(name+'.ai.b', ['2pm1'], ['3dm2', '3dm1_4sm1'])

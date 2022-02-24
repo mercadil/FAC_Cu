6d653600025a7142619bfe2c94dc5f70db8d4580
Laurent Mercadier <laurent.mercadier@xfel.eu>
2022-02-24 17:30:39 +0100

    2p -> 3d of ionic species transition rates

--- Cu_test.py
+++ Cu_test.py
@@ -24,7 +24,7 @@ fac.Config('2p5 3p6 4s1 3d4', group='ik1_ii7')
 fac.Config('2p5 3p6 4s1 3d3', group='ik1_ii8')
 fac.Config('2p5 3p6 4s1 3d2', group='ik1_ii9')
 fac.Config('2p5 3p6 4s1 3d1', group='ik1_ii10')
-
+'''
 fac.Config('2p5 3p5 4s1 3d10', group='ik2_ii2')
 fac.Config('2p5 3p5 4s1 3d9', group='ik2_ii3')
 fac.Config('2p5 3p5 4s1 3d8', group='ik2_ii4')
@@ -34,7 +34,7 @@ fac.Config('2p5 3p5 4s1 3d5', group='ik2_ii7')
 fac.Config('2p5 3p5 4s1 3d4', group='ik2_ii8')
 fac.Config('2p5 3p5 4s1 3d3', group='ik2_ii9')
 fac.Config('2p5 3p5 4s1 3d2', group='ik2_ii10')
-'''
+
 fac.Config('2p5 3p4 4s1 3d10', group='ik3_ii3')
 fac.Config('2p5 3p4 4s1 3d9', group='ik3_ii4')
 fac.Config('2p5 3p4 4s1 3d8', group='ik3_ii5')
@@ -43,7 +43,7 @@ fac.Config('2p5 3p4 4s1 3d6', group='ik3_ii7')
 fac.Config('2p5 3p4 4s1 3d5', group='ik3_ii8')
 fac.Config('2p5 3p4 4s1 3d4', group='ik3_ii9')
 fac.Config('2p5 3p4 4s1 3d3', group='ik3_ii10')
-'''
+
 fac.Config('2p6 3p5 4s1 3d10', group='ik4_ii1')
 fac.Config('2p6 3p5 4s1 3d9', group='ik4_ii2')
 fac.Config('2p6 3p5 4s1 3d8', group='ik4_ii3')
@@ -54,7 +54,7 @@ fac.Config('2p6 3p5 4s1 3d4', group='ik4_ii7')
 fac.Config('2p6 3p5 4s1 3d3', group='ik4_ii8')
 fac.Config('2p6 3p5 4s1 3d2', group='ik4_ii9')
 fac.Config('2p6 3p5 4s1 3d1', group='ik4_ii10')
-'''
+
 fac.Config('2p5 3p3 4s1 3d10', group='ik5_ii4')
 fac.Config('2p5 3p3 4s1 3d9', group='ik5_ii5')
 fac.Config('2p5 3p3 4s1 3d8', group='ik5_ii6')
@@ -65,30 +65,57 @@ fac.Config('2p5 3p3 4s1 3d4', group='ik5_ii10')
 '''
 
 #ladders = {0: 0, 1: 1, 2: 2, 3: 3, 4: 1, 5: 4}
-ladders = {0: 0, 1: 1, 2: 2, 4: 1}#, 5: 4}
+ladders = {0: 0, 1: 1, }#2: 2, 3: 3, 4: 1, 5: 4}
 
 all_groups = ["ik%d_ii%d"%(k, i) for k in ladders for
                i in range(ladders[k], 11)]
-#all_groups = ['ik0_ii0', 'ik0_ii1', 'ik1_ii1', 'ik1_ii2']
 print(all_groups)
 ii1_low = ['ik0_ii1']
 ii1_up = ['ik1_ii1', 'ik4_ii1']
-ii2_low = ['ik0_ii2']
-ii2_up = ['ik1_ii2', 'ik2_ii2']
+ii2_low = ['ik0_ii2', ]
+ii2_up = ['ik1_ii2', 'ik2_ii2', 'ik4_ii2']
+ii3_low = ['ik0_ii3',]
+ii3_up = ['ik1_ii3', 'ik2_ii3', 'ik3_ii3', 'ik4_ii3']
 
 name = 'Cu_test'
 fac.ConfigEnergy(0)
 fac.OptimizeRadial(['ik0_ii0'])
 fac.ConfigEnergy(1)
 print('ConfigEnergy and optimizeRadial done.')
-#fac.Structure(name+'.lev.b', all_groups)
+fac.Structure(name+'.lev.b', all_groups)
 fac.MemENTable(name+'.lev.b')
 fac.PrintTable(name+'.lev.b', name+'.lev', 1)
 print('Level table saved.')
-fac.TransitionTable(name+'.tr.b', ii1_low, ii1_up)
-fac.TransitionTable(name+'.tr.b', ii2_low, ii2_up)
+
+
+low_up = [['ik0_ii1', 'ik1_ii1'],
+['ik0_ii2', 'ik1_ii2'],
+['ik0_ii3', 'ik1_ii3'],
+['ik0_ii4', 'ik1_ii4'],
+['ik0_ii5', 'ik1_ii5'],
+['ik0_ii6', 'ik1_ii6'],
+['ik0_ii7', 'ik1_ii7'],
+['ik0_ii8', 'ik1_ii8'],
+['ik0_ii9', 'ik1_ii9'],
+['ik0_ii10', 'ik1_ii10']
+]
+for lu in low_up:
+    print(lu)
+    fac.TransitionTable(name+'.tr.b', [lu[0]], [lu[1]])
 fac.PrintTable(name+'.tr.b', name+'.tr', 1)
 print('Transition table saved.')
+
+
+#photoionization
+low_up = [['ik0_ii0', 'ik1_ii1'],
+           ['ik0_ii1', 'ik1_ii2']]
+for lu in low_up:
+    print(lu)
+    fac.SetUsrPEGrid([900, 930, 940, 950, 1000])
+    fac.RRTable(name+'.rr.b', [lu[0]], [lu[1]])
+fac.PrintTable(name+'.rr.b', name+'.rr', 1)
+print('RR table saved.')
+
 #fac.AITable(name+'.ai.b', ['2pm1'], ['3dm2', '3dm1_4sm1'])
 #fac.PrintTable(name+'.ai.b', name+'.ai', 1)
 #print('AI table saved.')
